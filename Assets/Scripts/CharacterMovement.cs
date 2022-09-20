1f60c5e7d86c17a6fcf28bdb0ced23ece00b6657
samuel ip <samuel.c.ip@student.uts.edu.au>
2022-09-20 19:50:31 +1000

    70% complete

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -7,7 +7,7 @@ public class CharacterMovement : MonoBehaviour
 
     private Vector3 movement;
     private float movemntSqrMagnitude;
-    
+    public float walkSpeed = 1.75f;
 
     
     void Update()
@@ -21,15 +21,24 @@ public class CharacterMovement : MonoBehaviour
 
     public void GetMovementInput()
     {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.z = Input.GetAxis("Vertical");
+        movement = Vector3.ClampMagnitude(movement,1.0f);
+        movemntSqrMagnitude = movement.sqrMagnitude;
+        Debug.Log(movement);
 
     }
     public void CharacterPosition()
     {
-
+        transform.Translate(movement * walkSpeed * Time.deltaTime,Space.World);
     }
     public void CharacterRotation()
     {
-
+        if(movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(movement, Vector3.up);
+        }
+        
     }
     public void WalkAnimation()
     {
